b8336baec75676877f16b94c06e53fed9fcb696a
lahirupiumsara <120963012+lahirupiumsara@users.noreply.github.com>
2024-07-25 02:03:36 +0530

    create divisions to set nav bar

---
+++ user/src/Components/Navigation/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+     <div className="navBarBox">
+
+        <div className="navBarLeft">
+            <span className="logo">facebook</span>
+        </div>
+        <div className="navBarCenter"></div>
+        <div className="navBarRight"></div>
+        
+        
+     </div>
+  )
+}
+
+export default NavBar
