739d0d486daa2f3e8758629a42a65300344bb43d
lahirupiumsara <120963012+lahirupiumsara@users.noreply.github.com>
2024-07-25 16:47:13 +0530

    90% complete nav bar

--- user/src/Components/Navigation/NavBar.jsx
+++ user/src/Components/Navigation/NavBar.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import MessageIcon from '@mui/icons-material/Message';
+import SettingsIcon from '@mui/icons-material/Settings';
+import './navBar.css'
 
 function NavBar() {
   return (
@@ -7,8 +12,39 @@ function NavBar() {
         <div className="navBarLeft">
             <span className="logo">facebook</span>
         </div>
-        <div className="navBarCenter"></div>
-        <div className="navBarRight"></div>
+
+        <div className="navBarCenter"> 
+             <div className="seachBarBox">
+               <SearchIcon className='searchIcon'/>
+               <input placeholder='search for your friends' className="searchInput" />
+               </div> 
+         </div>
+
+        <div className="navBarRight">
+            <div className="navBarLinks">
+                  <span className="navBarLink">HomePage</span>
+                  <span className="navBarLink">Profle</span>
+            </div>
+            <div className="navBarIcons">
+               <div className="navBarIcon">
+                  <PersonIcon/>
+                  <span className="iconTag">5</span>
+               </div>
+               <div className="navBarIcon">
+                  <MessageIcon/>
+                  <span className="iconTag">3</span>
+               </div>
+               <div className="navBarIcon">
+                  <SettingsIcon/>
+                  <span className="iconTag">2</span>
+               </div>
+              
+              <div className="pic"></div>
+              <img src="/images/1.jpg" alt="" className="profilePicImage" />
+
+            </div>
+
+        </div>
         
         
      </div>
